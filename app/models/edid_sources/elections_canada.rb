3be00e410895a631a23ff5185df7c14f0bc42a9f
Daniel Haran <chebuctonian@mgmail.com>
2010-06-06 16:50:12 -0400

    made scraper more robust; changes must be propagated (test is failing)

--- app/models/edid_sources/elections_canada.rb
+++ app/models/edid_sources/elections_canada.rb
@@ -4,18 +4,30 @@ module EdidSources
   class ElectionsCanada
     def self.edid_for(postal_code)
       postal_code.sub! ' ', '' # done in javascript on the site, "A1A1A1" is OK, "A1A 1A1" is NOT
+
+      search_results = scrape(postal_code)
       
+      if match = search_results.uri.to_s.match(/&ED=(\d+)&/)
+        [match[1]]
+      elsif search_results.content =~ /Your postal code information did not identify a valid electoral district/i
+        []
+      elsif search_results.content =~ /Your postal code identified more than one electoral district/i
+        ["multiple", search_results.uri]
+      else
+        raise "error scraping page for postal code: #{postal_code}"
+      end
+    end
+    
+    def self.scrape(postal_code)
       agent = Mechanize.new do |a|
         a.user_agent_alias = 'Mac Safari'
       end
 
       page = agent.get("http://www.elections.ca/home.asp")
       page.parser.encoding = 'utf8'
-      search_results =  page.form_with(:name => "POSTAL") do |lookup|
+      page.form_with(:name => "POSTAL") do |lookup|
         lookup.field_with(:name => "PC").value = postal_code
       end.submit
-
-      search_results.uri.to_s.match(/&ED=(\d+)&/)[1]
     end
   end
 end
---
+++ test/unit/edid_sources/elections_canada_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class EdidSources::ElectionsCanadaTest < ActiveSupport::TestCase
+  test "should return empty when no results are found" do
+    page = stub(:uri     => "http://www.elections.ca/scripts/pss/FindED.aspx?PC=H0H0H0&image.x=0&image.y=0",
+                :content => fixture_file("H0H0H0.html"))
+    EdidSources::ElectionsCanada.expects(:scrape).with("H0H0H0").returns(page)
+    
+    assert_equal [], EdidSources::ElectionsCanada.edid_for("H0H0H0")
+  end
+  
+  test "should a single result when possible" do
+    page = stub(:uri => "http://www.elections.ca/scripts/pss/InfovoteMain_ne.aspx?" +
+                        "L=e&ED=10007&EV=99&EV_TYPE=6&PC=A1A1A1&Prov=&ProvID=&MapID=&QID=-1&PageID=21&TPageID=")
+    EdidSources::ElectionsCanada.expects(:scrape).with("A1A1A1").returns(page)
+    
+    assert_equal ["10007"], EdidSources::ElectionsCanada.edid_for("A1A1A1")
+  end
+  
+  test "should return nil when more than 1 possibility exists" do
+    page = stub(:uri     => "http://www.elections.ca/scripts/pss/FindED.aspx?PC=T5S2B9&image.x=0&image.y=0",
+                :content => fixture_file("T5S2B9.html"))
+    EdidSources::ElectionsCanada.expects(:scrape).with("T5S2B9").returns(page)
+    
+    assert_equal ["multiple", "http://www.elections.ca/scripts/pss/FindED.aspx?PC=T5S2B9&image.x=0&image.y=0"], EdidSources::ElectionsCanada.edid_for("T5S2B9")
+  end
+end
